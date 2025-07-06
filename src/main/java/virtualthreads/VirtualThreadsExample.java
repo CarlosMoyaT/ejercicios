92ddfbcd7b0d6d60229b945ba05ea2f110398bcb
Carlos <solrak_27@hotmail.com>
2025-07-06 17:23:40 +0200

    Probando el lanzamiento de 100000 virtual threads

--- src/main/java/virtualthreads/VirtualThreadsExample.java
+++ src/main/java/virtualthreads/VirtualThreadsExample.java
@@ -1,13 +1,14 @@
 package virtualthreads;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class VirtualThreadsExample {
 
     public static void main(String[] args) {
 
-
         try {
             Thread.Builder builder = Thread.ofVirtual().name("worker-", 0);
-
             Runnable task = () -> {
                 System.out.println("Thread ID: " + Thread.currentThread().threadId());
             };
@@ -15,7 +16,6 @@ public class VirtualThreadsExample {
             // name "worker-0"
             Thread t1 = builder.start(task);
             t1.join();
-
             System.out.println(t1.getName() + " terminated");
 
             // name "worker-1
@@ -23,13 +23,26 @@ public class VirtualThreadsExample {
             t2.join();
             System.out.println(t2.getName() + " terminated");
 
-            // add delay 2 seconds
+            // Waiting two seconds before launch the Threads
             Thread.sleep(2000);
-
 
         }catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        Instant start = Instant.now();
+
+        // Launching 100.000 virtual threads
+        for (int i = 0; i < 100_000; i++) {
+            Thread.startVirtualThread(() -> {
+                System.out.println(Thread.currentThread());
+            });
+        }
+
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
+
+        System.out.println("Total time " + duration.toMillis() + " ms");
 
     }
 }
